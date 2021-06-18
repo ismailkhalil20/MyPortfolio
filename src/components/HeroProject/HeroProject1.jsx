23ef127c13c190e0afd374a7d5eba721b389a3b3
unknown <ismail.th.khalil@gmail.com>
2021-06-18 15:19:50 +0300

    Add second HeroProject

--- src/components/HeroProject/HeroProject1.jsx
+++ src/components/HeroProject/HeroProject1.jsx
@@ -9,7 +9,7 @@ export default function HeroProject1() {
                     <img className="object-cover object-center rounded" alt="hero" src={Dilmun} />
                 </div>
                 <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-orange-400">Dilmun
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-yellow-400">Dilmun
                         <br className="hidden lg:inline-block" />
                     </h1>
                     <p className="mb-8 leading-relaxed text-white">Online platfrom for connecting people who want to donate items or sell used products, users can create account and fill their information and post any product they want to doante or sell.</p>
--- src/components/HeroProject/HeroProject2.jsx
+++ src/components/HeroProject/HeroProject2.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import WeatherApp from '../../assets/WeatherApp.png'
 
 export default function HeroProject2() {
     return (
-        <div>
-            
-        </div>
+        <section className="text-gray-600 body-font">
+            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col-reverse items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-yellow-400">Weather application
+                        <br className="hidden lg:inline-block" />
+                    </h1>
+                    <p className="mb-8 leading-relaxed text-white">An online website for checking the weather condition for a specific location you are searching for, you can also see the weather forecast for the next few days.</p>
+                    <div className="flex justify-center">  
+                        <a href="https://github.com/ismailkhalil20/weather_app_js" className="inline-flex text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-white hover:text-black rounded text-lg">Github</a>
+                    </div>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-16">
+                    <img className="object-cover object-center rounded" alt="hero" src={WeatherApp} />
+                </div>
+            </div>
+        </section>
     )
 }
