69decc374799e3e1e9b947fd544d0dee0a071ec4
unknown <ismail.th.khalil@gmail.com>
2021-06-22 00:06:33 +0300

    Add the emailjs functionality

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,65 +17,65 @@ export default function About() {
                     <p className=" w-full leading-relaxed text-white text-lg">here are some of the technical skills I can work on</p>
                 </div>
 
-                <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+                <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
 
-                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                        <div class="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
+                    <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
                             <img src={ReactLogo} alt="React logo" />
                         </div>
-                        <div class="flex-grow">
-                            <h2 class="text-white text-xl title-font font-medium mb-3">React</h2>
-                            <p class="leading-relaxed text-indigo-200">Create reusable user interfaces from scratch.</p>
+                        <div className="flex-grow">
+                            <h2 className="text-white text-xl title-font font-medium mb-3">React</h2>
+                            <p className="leading-relaxed text-indigo-200">Create reusable user interfaces from scratch.</p>
                         </div>
                     </div>
 
-                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                        <div class="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
+                    <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
                            <img src={ReduxLogo} alt="Redux Logo" />
                         </div>
-                        <div class="flex-grow">
-                            <h2 class="text-white text-xl title-font font-medium mb-3">Redux</h2>
-                            <p class="leading-relaxed text-indigo-200">Manage the state of the application by centralizing it to get access to the same data across the application.</p>
+                        <div className="flex-grow">
+                            <h2 className="text-white text-xl title-font font-medium mb-3">Redux</h2>
+                            <p className="leading-relaxed text-indigo-200">Manage the state of the application by centralizing it to get access to the same data across the application.</p>
                         </div>
                     </div>
 
-                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                        <div class="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
+                    <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
                             <img src={TailwindLogo} alt="Tailwind Logo" />
                         </div>
-                        <div class="flex-grow">
-                            <h2 class="text-white text-xl title-font font-medium mb-3">Tailwind</h2>
-                            <p class="leading-relaxed text-indigo-200">style the web pages with Tailwind which is a utility-first CSS Framework packed with classes.</p>
+                        <div className="flex-grow">
+                            <h2 className="text-white text-xl title-font font-medium mb-3">Tailwind</h2>
+                            <p className="leading-relaxed text-indigo-200">style the web pages with Tailwind which is a utility-first CSS Framework packed with classNamees.</p>
                         </div>
                     </div>
 
-                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                        <div class="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
+                    <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
                             <img src={FirebaseLogo} alt="Firebase logo" />
                         </div>
-                        <div class="flex-grow">
-                            <h2 class="text-white text-xl title-font font-medium mb-3">Firebase</h2>
-                            <p class="leading-relaxed text-indigo-200">Send and retrieve data from firebase.</p>
+                        <div className="flex-grow">
+                            <h2 className="text-white text-xl title-font font-medium mb-3">Firebase</h2>
+                            <p className="leading-relaxed text-indigo-200">Send and retrieve data from firebase.</p>
                         </div>
                     </div>
 
-                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                        <div class="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
+                    <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
                         <img src={HTMLLogo} alt="HTML Logo" />
                         </div>
-                        <div class="flex-grow">
-                            <h2 class="text-white text-xl title-font font-medium mb-3">HTML 5</h2>
-                            <p class="leading-relaxed text-indigo-200">create web pages using HTML5 tags.</p>
+                        <div className="flex-grow">
+                            <h2 className="text-white text-xl title-font font-medium mb-3">HTML 5</h2>
+                            <p className="leading-relaxed text-indigo-200">create web pages using HTML5 tags.</p>
                         </div>
                     </div>
 
-                    <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                        <div class="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
+                    <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                        <div className="w-32 h-32 inline-flex items-center justify-center rounded-full text-indigo-500 mb-5 flex-shrink-0">
                             <img src={CSSLogo} alt="CSS Logo" />
                         </div>
-                        <div class="flex-grow">
-                            <h2 class="text-white text-xl title-font font-medium mb-3">CSS 3</h2>
-                            <p class="leading-relaxed text-indigo-200">use CSS properties to style the HTML elements of the web page.</p>
+                        <div className="flex-grow">
+                            <h2 className="text-white text-xl title-font font-medium mb-3">CSS 3</h2>
+                            <p className="leading-relaxed text-indigo-200">use CSS properties to style the HTML elements of the web page.</p>
                         </div>
                     </div>
 
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
 import TwitterLogo from '../../assets/Twitter.svg'
 import LinkedinLogo from '../../assets/linkedin.svg'
 import GithubLogo from '../../assets/github.svg'
 
 export default function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+        console.log(e.target.name)
+    
+        emailjs.sendForm('service_0b54z4t', 'template_el7l1sw', e.target, 'user_6L5Ozd4rfDtSiJcdbDOb8')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      }
+
     return (
         <section className="text-gray-600 body-font relative">
             <div className="container px-5 py-24 mx-auto">
@@ -11,48 +26,50 @@ export default function Contact() {
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-yellow-400">Contact</h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-white">If you want to know more about me or you have a question, please feel free to send me a message.</p>
                 </div>
+                <form onSubmit={sendEmail}>
+                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                        <div className="flex flex-wrap -m-2">
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label htmlFor="name" className="leading-7 text-sm text-indigo-300">Name</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label htmlFor="email" className="leading-7 text-sm text-indigo-300">Email</label>
+                                    <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-2 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label htmlFor="message" className="leading-7 text-sm text-indigo-300">Message</label>
+                                    <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                </div>
+                            </div>
+                            
+                            <div className="p-2 w-full">
+                                <button type="submit" className="flex mx-auto text-white bg-yellow-400 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-500 rounded text-lg">Send</button>
+                            </div>
 
-                <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                    <div className="flex flex-wrap -m-2">
-                        <div className="p-2 w-1/2">
-                            <div className="relative">
-                                <label for="name" className="leading-7 text-sm text-indigo-300">Name</label>
-                                <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                                <p className="leading-normal my-5 text-lg text-yellow-400">You can find me on.
+                                </p>
+                                <span className="inline-flex space-x-4">
+                                    <a href="https://twitter.com/IsmailKhalil_" className="w-6 h-6">
+                                        <img src={TwitterLogo} alt="Twitter logo" />
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/ismailkhalil20/" className="w-6 h-6">
+                                        <img src={LinkedinLogo} alt="Twitter logo" />
+                                    </a>
+                                    <a href="https://github.com/ismailkhalil20" className="w-6 h-6">
+                                        <img src={GithubLogo} alt="Twitter logo" />
+                                    </a>
+                                </span>
                             </div>
-                        </div>
-                        <div className="p-2 w-1/2">
-                            <div className="relative">
-                                <label for="email" className="leading-7 text-sm text-indigo-300">Email</label>
-                                <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-2 leading-8 transition-colors duration-200 ease-in-out" />
-                            </div>
-                        </div>
-                        <div className="p-2 w-full">
-                            <div className="relative">
-                                <label for="message" className="leading-7 text-sm text-indigo-300">Message</label>
-                                <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                            </div>
-                        </div>
-                        <div className="p-2 w-full">
-                            <button className="flex mx-auto text-white bg-yellow-400 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-500 rounded text-lg">Send</button>
-                        </div>
-
-                        <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                            <p className="leading-normal my-5 text-lg text-yellow-400">You can find me on.
-                            </p>
-                            <span className="inline-flex space-x-4">
-                                <a href="https://twitter.com/IsmailKhalil_" className="w-6 h-6">
-                                    <img src={TwitterLogo} alt="Twitter logo" />
-                                </a>
-                                <a href="https://www.linkedin.com/in/ismailkhalil20/" className="w-6 h-6">
-                                    <img src={LinkedinLogo} alt="Twitter logo" />
-                                </a>
-                                <a href="https://github.com/ismailkhalil20" className="w-6 h-6">
-                                    <img src={GithubLogo} alt="Twitter logo" />
-                                </a>
-                            </span>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </section>
     )
